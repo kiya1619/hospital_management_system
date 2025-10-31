e59638d08ac3b221083cf9a01f3a105d14ae60d9
unknown <abdisaworkmail@gmail.com>
2025-10-31 15:01:55 +0300

    ddsadsss

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -85,7 +85,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-allowed_hosts = ['abdisa16.pythonanywhere.com']
+ALLOWED_HOSTS = ['abdisa16.pythonanywhere.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
